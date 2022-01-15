324bdcc813478c914ee7b1197d23995c30c1b82d
sahars <hosseinzadeh.sahar.h@gmail.com>
2022-01-15 12:46:03 +0100

    Log the Type of

---
+++ week1/homework/logType.js
@@ -0,0 +1,47 @@
+/*function checkTheType (one, two);
+if (typeof one == typeof two) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}*/
+
+let one = "Famous Blue Raincoat";
+let two = "Avalanch";
+let three = 2010;
+let four = 2020;
+
+if (typeof one == typeof two) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
+
+if (typeof one == typeof three) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
+
+if (typeof one == typeof four) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
+
+if (typeof two == typeof three) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
+
+if (typeof two == typeof four) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
+
+if (typeof three == typeof four) {
+    console.log("SAME TYPE")
+} else {
+    console.log("Not the same")
+}
