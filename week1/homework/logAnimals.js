79cad7a91d5539f14b72ee225a64cafe15a642e9
sahars <hosseinzadeh.sahar.h@gmail.com>
2022-01-15 12:22:54 +0100

    Lag an Array

---
+++ week1/homework/logAnimals.js
@@ -0,0 +1,16 @@
+let items = [];
+console.log("My favorite animals are");
+console.log(items);
+
+let favAnimals = ["Cat", "Panda", "Quala"];
+console.log(favAnimals);
+
+let newItem = favAnimals.push("Piglet");
+console.log(favAnimals);
+
+console.log(favAnimals[2]);
+
+
+
+
+
---
+++ week1/homework/logRound.js
@@ -0,0 +1,7 @@
+const z = 7.25;
+console.log(z);
+
+const a = Math.round(z);
+console.log(a);
+
+console.log(Math.max(a, z));
