5e1052e9eade8da44a5e02d1213d2a5f091fc84c
sahars <hosseinzadeh.sahar.h@gmail.com>
2022-01-15 13:54:31 +0100

    log the arrays length

---
+++ week1/homework/logArrays.js
@@ -0,0 +1,9 @@
+const days = ["Saturday", "Sunday", "Monday", "Tuesday"];
+const months = ["April", "March", "May"];
+console.log("The length af the arrays is " + days.length + months.length);
+
+if (days.length == months.length) {
+    console.log ("They are the same")
+} else {
+    console.log("Two different sizes")
+}
