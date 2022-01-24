eb6ab335e53173e67f7c32582327e057276c88dc
sahars <hosseinzadeh.sahar.h@gmail.com>
2022-01-24 16:23:04 +0100

    The even/odd reporter

---
+++ week2/homework/js-exercises/reporter.js
@@ -0,0 +1,7 @@
+for (let num = 0; num <= 20; num++) {
+    if (num % 2 != 0) {
+        console.log("The number " + num + "is odd");
+    } else {
+        console.log("The number " + num + "is even");
+    }
+}
